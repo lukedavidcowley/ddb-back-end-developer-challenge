afb4b1089a1751676e8c923305937c48b142e1e6
lukedavidcowley <lukedavidcowley@gmail.com>
2020-07-20 12:10:09 -0400

    feature tests and bll init

---
+++ ProductName/GameEngine.Tests/Controllers/CharacterControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace GameEngine.Tests.Controllers
+{
+    [TestFixture]
+    public class CharacterControllerTests
+    {
+        [Test]
+        public void CanAddCharacterToGame()
+        {
+            throw new NotImplementedException();
+        }
+        [Test]
+        public void CanUpdateCharacter()
+        {
+            throw new NotImplementedException();
+        }
+        [Test]
+        public void CanAddTemporaryHitPoints()
+        {
+            throw new NotImplementedException();
+        }
+        [Test]
+        public void CanHeal()
+        {
+
+        }
+
+    }
+}
---
+++ ProductName/GameEngine.Tests/Controllers/GameControllerTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine.Tests.Controllers.Game
+{
+    [TestFixture]
+    public class GameControllerTests
+    {
+        [Test]
+        public void CanCreateGame()
+        {
+            throw new NotImplementedException();
+        }
+        [Test]
+        public void CanCloseGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
