43337429a476da65b4eb4fb76968ee0084d32675
lukedavidcowley <lukedavidcowley@gmail.com>
2020-07-27 13:49:32 -0400

    character controller tests and scaffold

--- ProductName/GameEngine.Tests/Controllers/CharacterControllerTests.cs
+++ ProductName/GameEngine.Tests/Controllers/CharacterControllerTests.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using ProductName.Business.Models;
+using ProductName.GameEngine.Controllers;
 using System;
 
 namespace GameEngine.Tests.Controllers
@@ -9,12 +11,39 @@ namespace GameEngine.Tests.Controllers
         [Test]
         public void CanAddTemporaryHitPoints()
         {
-            throw new NotImplementedException();
+            //assemble
+            var controller = new CharacterController();
+
+            //act
+            var result = controller.AddTemporaryHitPoints(Guid.NewGuid(), 1);
+
+            //assert
+            Assert.IsTrue(result);
         }
         [Test]
         public void CanHeal()
         {
+            //assemble
+            var controller = new CharacterController();
 
+            //act
+            var result = controller.AddHitPoints(Guid.NewGuid(), 1);
+
+            //assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanAttack()
+        {
+            //assemble
+            var controller = new CharacterController();
+
+            //act
+            var result = controller.Attack(Guid.NewGuid(), 1, DamageType.Acid);
+
+            //assert
+            Assert.IsTrue(result);
         }
 
     }
---
+++ ProductName/ProductName.GameEngine/Controllers/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductName.Business.Models;
+
+namespace ProductName.GameEngine.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+
+        [Route("{characterId}/hp/add")]
+        public bool AddHitPoints(Guid characterId, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{characterId}/hp/add/temporary")]
+        public bool AddTemporaryHitPoints(Guid characterId, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{characterId}/attack")]
+        public bool Attack(Guid characterId, int value, DamageType damageType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
